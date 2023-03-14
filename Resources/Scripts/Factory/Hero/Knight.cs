755423ddfae762fb475a2a710deb5f1d0323f526
Golde34 <82593925+Golde34@users.noreply.github.com>
2023-03-14 08:21:43 +0700

    update hero
    update hero

---
+++ Resources/Scripts/Factory/Hero/Knight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Knight : MonoBehaviour, IHero
+{
+    public float Health { get; set; }
+    public int Damage { get; set; }
+    public int Defense { get; set; }
+    public float Speed { get; set; }
+    public float DSpeed { get; set; }
+    public float Range { get; set; }
+    public float currentHealth { get; set; }
+
+    void Update()
+    {
+        GameObject target = GameObject.FindGameObjectWithTag("Hero");
+        gameObject.transform.position = target.transform.position;
+    }
+
+    public void Attack(Collider2D[] hitEnemies)
+    {
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            if (enemy.tag.Equals("Enemy"))
+            {
+                enemy.GetComponent<Enemy>().takeDamage(Damage);
+            }
+        }
+    }
+
+    public void takeDamage(float amount)
+    {
+        currentHealth -= amount;
+
+        Debug.Log("Current Heath: " + currentHealth+"/" + Health + "; Loss: " + amount);
+
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Resources/Scripts/Factory/Hero/KnightFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class KnightFactory : IHeroFactory
+{
+    public IHero CreateHero()
+    {
+        GameObject knightObj = new GameObject("Knight");
+        Knight knight = knightObj.AddComponent<Knight>();
+        knight.Health = 180;
+        knight.currentHealth = knight.Health;
+        knight.Damage = 20;
+        knight.Defense = 15;
+        knight.Speed = 1;
+        knight.DSpeed = 1;
+        knight.Range = 0.2f;
+        return knight;
+    }
+}
